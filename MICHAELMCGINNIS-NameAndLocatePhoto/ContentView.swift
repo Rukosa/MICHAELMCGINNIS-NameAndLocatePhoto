69ea2175965f85240ad62d11a5f7a43e1e5f271c
Rukosa <mwmcginnis490@gmail.com>
2022-04-18 15:03:56 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-NameAndLocatePhoto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MICHAELMCGINNIS-NameAndLocatePhoto
+//
+//  Created by Michael Mcginnis on 4/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MICHAELMCGINNIS-NameAndLocatePhoto/MICHAELMCGINNIS_NameAndLocatePhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_NameAndLocatePhotoApp.swift
+//  MICHAELMCGINNIS-NameAndLocatePhoto
+//
+//  Created by Michael Mcginnis on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_NameAndLocatePhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
